d184eff1ee4e03d47ff8930ad3b413f01d6b0a8b
Gage Lieble <gagelieble@hotmail.com>
2022-08-09 14:21:16 -0700

    Image Slider Controls with auto slide

--- VanillaJS-projects/ImageSliderWControl/slider.js
+++ VanillaJS-projects/ImageSliderWControl/slider.js
@@ -1,24 +1,23 @@
 
+// Buttons
 let left = document.getElementById('left')
 let right = document.getElementById('right')
 
+// Params
 let start = 0;
 let images =[]
 let time = 3000;
+let slide = document.getElementById('slide')
 
-
-
+// Images
 images[0] = 'imgs/1.jpg'
 images[1] = 'imgs/2.jpg'
 images[2] = 'imgs/3.jpg'
 images[3] = 'imgs/4.jpg'
 
-let slide = document.getElementById('slide')
-
-
+// Functions
 left.addEventListener('click', function(){
     start -= 1
-    
     if (start < 0){
         start = 3
         slide.src = images[3]
@@ -27,35 +26,26 @@ left.addEventListener('click', function(){
         
         slide.src = images[start]
     }
-
-    
-    
 })
 
 right.addEventListener('click', function(){
-
     if (start > 2){
             start = -1
     }
     start += 1
     slide.src = images[start]
-    
-    
 })
 
-
-
-// function changeImg(){
-//     slide.src = images[start]
-
-//     if (start < images.length - 1){
-//         start ++ 
-//     }
-//     else {
-//         start = 0
-//     }
-//     setTimeout('changeImg()', time)
-// }
-
-// changeImg()
+// Auto Slide
+function changeImg(){
+    slide.src = images[start]
+    if (start < images.length - 1){
+        start ++ 
+    }
+    else {
+        start = 0
+    }
+    setTimeout('changeImg()', time)
+}
+changeImg()
     
